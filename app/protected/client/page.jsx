5711d4d8c4ca18213f28062afb20ae90246eb195
JonathanArriazu <joni.arriazu2@gmail.com>
2023-08-08 18:04:28 -0300

    fix: show user name in client-side and server-side components

--- app/protected/client/page.jsx
+++ app/protected/client/page.jsx
@@ -1,14 +1,16 @@
 'use client'
+import { useSession } from 'next-auth/react'
 import React from 'react'
 
 const ProtectedClientPage = () => {
+  const {data: session} = useSession()
   return (
     <div>
         <h1>This is a
             <i style={{color: 'red'}}> Client-Side</i> protected page
         </h1>
 
-        <p>You are logged in as: <b>JoniArriazu</b></p>
+        <p>You are logged in as: <b>{session?.user?.name}</b></p>
     </div>
   )
 }
--- app/protected/server/page.jsx
+++ app/protected/server/page.jsx
@@ -1,14 +1,16 @@
 'use server'
 import React from 'react'
+import { getServerSession } from 'next-auth/next'
 
-const ProtectedServerPage = () => {
+const ProtectedServerPage = async () => {
+  const session = await getServerSession()
   return (
     <div>
         <h1>This is a
             <i style={{color: 'red'}}> Server-Side</i> protected page
         </h1>
 
-        <p>You are logged in as: <b>JoniArriazu</b></p>
+        <p>You are logged in as: <b>{session?.user?.name}</b></p>
     </div>
   )
 }
